a1a9cbefee75a7d236182ae38ff1d941effc907d
Kyle Cannoles <kylecann@hawaii.edu>
2025-02-19 11:45:14 +0900

    Move font config to modules/fonts.nix and QoL changes

--- machines/laptop/configuration.nix
+++ machines/laptop/configuration.nix
@@ -9,7 +9,7 @@
     [ 
       ./hardware-configuration.nix
       ../../modules/core.nix
-      #../../modules/fonts.nix
+      ../../modules/fonts.nix
       ../../modules/hyprland.nix
       ../../modules/packages.nix
       ../../modules/programs.nix
@@ -25,10 +25,13 @@
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
   programs.nm-applet.enable = true; # System tray icon
 
-  # iPhone
+  # iPhone tethering
   services.usbmuxd = {
     enable = true;
     package = pkgs.usbmuxd2;
+  };
+  systemd.services.usbmuxd.serviceConfig = {
+    TimeoutSec = 5;
   };
   environment.systemPackages = with pkgs; [
     libimobiledevice
@@ -61,7 +64,11 @@
       settings = {
         main = {
           # overloads the capslock key to function as both escape (when tapped) and backspace (when held)
-          capslock = "overload(backspace, esc)";
+          #capslock = "overload(backspace, esc)";
+          capslock = "backspace";
+        };
+        shift = {
+          capslock = "capslock";
         };
       };
     };
@@ -111,6 +118,7 @@
   security.pam.services.greetd.enableGnomeKeyring = true;
   # Enable touchpad support (enabled default in most desktopManager).
   services.libinput.enable = true;
+  services.libinput.touchpad.disableWhileTyping = true;
 
   xdg.portal = {
     enable = true;
@@ -127,53 +135,6 @@
     ];
   };
 
-  # all fonts are linked to /nix/var/nix/profiles/system/sw/share/X11/fonts
-  fonts = {
-    # use fonts specified by user rather than default ones
-    enableDefaultPackages = false;
-    fontDir.enable = true;
-
-    packages = with pkgs; [
-      # icon fonts
-      material-design-icons
-      font-awesome
-
-      # Noto 系列字体是 Google 主导的，名字的含义是「没有豆腐」（no tofu），因为缺字时显示的方框或者方框被叫作 tofu
-      # Noto 系列字族名只支持英文，命名规则是 Noto + Sans 或 Serif + 文字名称。
-      # 其中汉字部分叫 Noto Sans/Serif CJK SC/TC/HK/JP/KR，最后一个词是地区变种。
-      noto-fonts # 大部分文字的常见样式，不包含汉字
-      noto-fonts-cjk-sans # 汉字部分
-      noto-fonts-emoji # 彩色的表情符号字体
-      noto-fonts-extra # 提供额外的字重和宽度变种
-
-      # 思源系列字体是 Adobe 主导的。其中汉字部分被称为「思源黑体」和「思源宋体」，是由 Adobe + Google 共同开发的
-      source-sans # 无衬线字体，不含汉字。字族名叫 Source Sans 3 和 Source Sans Pro，以及带字重的变体，加上 Source Sans 3 VF
-      source-serif # 衬线字体，不含汉字。字族名叫 Source Code Pro，以及带字重的变体
-      source-han-sans # 思源黑体
-      source-han-serif # 思源宋体
-
-      # nerdfonts
-      (nerdfonts.override {
-        fonts = [
-          "FiraCode"
-          "JetBrainsMono"
-          "Iosevka"
-        ];
-      })
-
-      #(pkgs.callPackage ../../fonts/icomoon-feather-icon-font.nix {})
-    ];
-
-    # user defined fonts
-    # the reason there's Noto Color Emoji everywhere is to override DejaVu's
-    # B&W emojis that would sometimes show instead of some Color emojis
-    fontconfig.defaultFonts = {
-      serif = ["Noto Serif" "Noto Color Emoji"];
-      sansSerif = ["Noto Sans" "Noto Color Emoji"];
-      monospace = ["JetBrainsMono Nerd Font" "Noto Color Emoji"];
-      emoji = ["Noto Color Emoji"];
-    };
-  };
 
   environment.variables = {
     # fix https://github.com/NixOS/nixpkgs/issues/238025
